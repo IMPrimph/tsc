454b424599c763e4ae2a12e2a06d00f570a1cd39
G Vishnu sai jaswanth <g.vishnu.sai.jaswanth@hp.com>
2022-06-10 11:26:00 +0530

    variable inference and any type

--- featuresOfTs/annotations/variables.ts
+++ featuresOfTs/annotations/variables.ts
@@ -31,3 +31,31 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
     console.log(i);
 }
+
+// when to use any annotation
+// 1) Function that returns the any type
+// as the json values can be of any type, typescript cant predict them
+// so it assigns a general type called "any" for json variables
+const json = '{"x": 10, "y": 20}'
+const coordinates: {x: number; y: number} = JSON.parse(json)
+console.log(coordinates)
+
+// 2) when we declare a variable on one line and initialize it later
+let words = ['red', 'blue', 'green']
+let foundWord: boolean
+
+for (let i = 0; i < words.length; i++){
+    if(words[i] == 'green'){
+        foundWord = true
+    }
+}
+
+// 3) Variables whose type cant be inferred correctly
+let numbers = [-10, -1, 12]
+let numberAboveZero: boolean | number = false
+
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] > 0){
+        numberAboveZero = numbers[i]
+    }
+}
